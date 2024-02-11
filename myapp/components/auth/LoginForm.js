8f22d65bb619a26c8108719b8cd7d0d0fe3fa7ab
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-02-11 15:09:57 -0500

    update

--- myapp/components/auth/LoginForm.js
+++ myapp/components/auth/LoginForm.js
@@ -2,20 +2,15 @@
 import React from 'react';
 import BoxWrapper from './BoxWrapper';
 import { useForm } from 'react-hook-form';
-import { LoginSchema } from '@/lib/schemas'; // Import your Zod schema
 import { Button, Input } from "@nextui-org/react";
+import { LoginSchema } from '@/lib/schemas';
 
 const LoginForm = () => {
-  const { handleSubmit, control, formState: { errors } } = useForm();
-  
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
   const onSubmit = (data) => {
-    try {
-      // Validate form data using Zod schema
-      const validatedData = LoginSchema.parse(data);
-      console.log(validatedData); // Log validated data to console
-    } catch (error) {
-      console.error(error.errors); // Log validation errors to console
-    }
+    console.log(data);
+    // Your login logic here
   };
 
   return (
@@ -26,30 +21,26 @@ const LoginForm = () => {
       backButtonLabel={'Back To Home'}
     >
       <form onSubmit={handleSubmit(onSubmit)} className='space-y-6'>
-  <div className='space-y-4'>
-    <Input
-      type="email"
-      name='email' // Match this with the schema key 'email'
-      label="Email"
-      placeholder='jace.jace@gmail.com'
-      control={control}
-      autoComplete='off'
-    />
-    {errors.email && <span className="text-red-500">{errors.email.message}</span>}
-  </div>
-  <div className='space-y-4'>
-    <Input
-      type="password"
-      name='password' // Match this with the schema key 'password'
-      label="Password"
-      control={control}
-      autoComplete='off'
-    />
-    {errors.password && <span className="text-red-500">{errors.password.message}</span>}
-  </div>
-  <Button className='w-full' type='submit'>Login</Button>
-</form>
-
+        <div className='space-y-4'>
+          <Input
+            type="email"
+            {...register('email', { required: 'Email is required', pattern: { value: LoginSchema.email } })}
+            label="Email"
+            autoComplete='off'
+          />
+          {errors.email && <span className="text-red-500">{errors.email.message}</span>}
+        </div>
+        <div className='space-y-4'>
+          <Input
+            type="password"
+            {...register('password', { required: 'Password is required' })}
+            label="Password"
+            autoComplete='off'
+          />
+          {errors.password && <span className="text-red-500">{errors.password.message}</span>}
+        </div>
+        <Button className='w-full' type='submit'>Login</Button>
+      </form>
     </BoxWrapper>
   );
 }
