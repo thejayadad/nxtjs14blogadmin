5161bdd0556427d7df55ae4fea0e70d3fdec8e63
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-02-11 18:49:10 -0500

    update

---
+++ myapp/middleware.js
@@ -0,0 +1,17 @@
+
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+    const isLoggedIn = !!req.auth;
+    console.log("ROUTE: ", req.nextUrl.pathname)
+    console.log("Is LoggedIn: ", isLoggedIn)
+})
+
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  }
